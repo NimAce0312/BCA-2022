f98ef315e6aea3793169b561e7075e93d4957fb6
Nimesh Pradhanang <nimesh.pradhanang@gmail.com>
2023-01-22 19:38:55 +0545

    Gallery Page

--- ShutterScape/JS/main.js
+++ ShutterScape/JS/main.js
@@ -1,7 +1,13 @@
 const menuToggler = document.querySelector('.menu-toggle');
 const menuContents = document.querySelector('.menu-contents');
+const galleryMenu = document.querySelector('.kebab');
+const galleryCategory = document.querySelector('.category-container');
 
 menuToggler.addEventListener("click",function(){
     menuToggler.classList.toggle('active');
     menuContents.classList.toggle('active');
+});
+
+galleryMenu.addEventListener("click",function(){
+    galleryCategory.classList.toggle('open');
 });
